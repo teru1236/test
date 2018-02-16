b11cecd4a23ee6af861fe9bcdc766426fb13fdb1
teru1236 <virno1236@gmail.com>
2018-02-16 17:58:27 +0900

    2018/2/16_end

---
+++ 4each_keijiban/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>掲示板</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+    <body>
+    <img src="4eachblog_logo.jpg">
+    <header>
+      <div class="menu">
+        <ul>
+          <li>トップ</li>
+          <li>プロフィール</li>
+          <li>4eachについて</li>
+          <li>登録フォーム</li>
+          <li>問い合わせ</li>
+          <li>その他</li>
+        </ul>
+      </div>
+    </header>
+        <main>
+            <div class="content">
+                <div class="maincontent">
+                    <h1>プログラミングに役立つ書籍</h1>
+                    <p>2017年1月15日</p>
+                    <div class="image1">
+                    <img src="bookstore.jpg" width="100%" height="100%">
+                    </div>
+                    
+                    <div class="side">
+                        <h2>人気の記事</h2>
+                        <p>PHP オススメ本</p>
+                        <p>PHP MyAdminの使い方</p>
+                        <p>今人気のエディタTop5</p>
+                        <p>HTMLの基礎</p>
+                        
+                        <h2>オススメリンク</h2>
+                        <p>XAMPPのダウンロード</p>
+                        <p>Eclipseのダウンロード</p>
+                        <p>Bracketsのダウンロード</p>
+
+                        <h2>カテゴリ</h2>
+                        <p>HTML</p>
+                        <p>PHP</p>
+                        <p>MySQL</p>
+                        <p>JavaScript</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    <footer>
+        <p>copyrihgt &copy; internous | 4each blog is the one which provides A to Z about programming.</p>
+        </footer>
+  </body>
+</html>
