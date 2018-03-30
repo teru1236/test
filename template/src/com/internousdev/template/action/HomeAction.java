652eb50f649ed08aa705ee9f9a05f6ae4fe9bc4a
uehara3737 <siza3737@yahoo.co.jp>
2018-03-30 16:42:24 +0900

    2ヶ月目終了

--- template/src/com/internousdev/template/action/HomeAction.java
+++ template/src/com/internousdev/template/action/HomeAction.java
@@ -1,30 +1,9 @@
 package com.internousdev.template.action;
 
-import java.util.Map;
-
-import org.apache.struts2.interceptor.SessionAware;
-
-import com.internousdev.template.dao.BuyItemDAO;
-import com.internousdev.template.dto.BuyItemDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
-public class HomeAction extends ActionSupport implements SessionAware{
-	public Map<String,Object> session;
+public class HomeAction extends ActionSupport{
 	public String execute(){
-		String result = "login";
-		if(session.containsKey("id")){
-			BuyItemDAO buyItemDAO = new BuyItemDAO();
-			BuyItemDTO buyItemDTO = buyItemDAO.getBuyItemInfo();
-			session.put("id", buyItemDTO.getId());
-			session.put("buyItem_name", buyItemDTO.getItemName());
-			session.put("buyItem_price", buyItemDTO.getItemPrice());
-
-			result = SUCCESS;
-		}
-		return result;
+		return SUCCESS;
 	}
-	public void setSession(Map<String, Object> session) {
-		this.session = session;
-	}
-
 }
